b7c4d99691b1342309eefd727c74fe3a456d4259
Reeggie Mace <reginaldmace36@gmail.com>
2021-03-29 21:11:51 -0400

    testing api call

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 console.log("connection");
 
+var ts = Date.now();
+console.log(ts);
+
+fetch(
+  "https://gateway.marvel.com:443/v1/public/characters?name=thor&apikey=22cb76a0a50613bcff0104d06cd9ec76",
+
+  {
+    referrer: "https://developer.marvel.com/fake.html",
+    // referrerPolicy: "origin",
+    mode: "cors",
+    credentials: "include",
+  }
+);
